909b2898e62162fc581ac939355adcf76f594895
mahfouzhsein <73946000+mahfouzhsein@users.noreply.github.com>
2026-09-07 22:21:33 +0300

    fix: avoid Storybook template formatter binding

--- app/components/coin/CoinChange.vue
+++ app/components/coin/CoinChange.vue
@@ -3,10 +3,11 @@ import { formatPercent } from '../../utils/formatters'
 
 const props = defineProps({ value: { type: Number, default: 0 } })
 const positive = computed(() => props.value >= 0)
+const formattedValue = computed(() => formatPercent(props.value))
 </script>
 
 <template>
   <span class="font-medium tabular-nums" :class="positive ? 'text-emerald-400' : 'text-rose-400'">
-    {{ formatPercent(value) }}
+    {{ formattedValue }}
   </span>
 </template>
