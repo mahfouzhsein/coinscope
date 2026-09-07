de92be1171249ab58e9eba2ac111852f97891418
mahfouzhsein <73946000+mahfouzhsein@users.noreply.github.com>
2026-09-07 22:08:20 +0300

    fix: give skeleton stories visible preview width

--- stories/BaseSkeleton.stories.js
+++ stories/BaseSkeleton.stories.js
@@ -1,5 +1,10 @@
 import BaseSkeleton from '../app/components/base/BaseSkeleton.vue'
 
-export default { title: 'Base/Skeleton', component: BaseSkeleton }
+export default {
+  title: 'Base/Skeleton',
+  component: BaseSkeleton,
+  decorators: [() => ({ template: '<div style="width: 28rem; max-width: 80vw"><story /></div>' })],
+}
+
 export const Single = { args: { lines: 1 } }
 export const List = { args: { lines: 6 } }
