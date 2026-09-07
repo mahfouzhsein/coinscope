f19c4f6147bacd6f14477f96e12534864370edb0
mahfouzhsein <73946000+mahfouzhsein@users.noreply.github.com>
2026-09-07 19:41:25 +0300

    fix: configure Vue SFC transform for Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,14 @@
+import vue from '@vitejs/plugin-vue'
+
 export default {
   stories: ['../stories/**/*.stories.js'],
   addons: [],
   framework: {
     name: '@storybook/vue3-vite',
-    options: {},
+    options: { docgen: 'vue-component-meta' },
+  },
+  async viteFinal(config) {
+    config.plugins = [...(config.plugins || []), vue()]
+    return config
   },
 }
